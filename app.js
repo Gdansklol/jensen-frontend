451b20bd8c70f1cf65aad7c14ee77732cd64bc96
Båmi Lee <swedenebomi@gmail.com>
2022-01-19 12:41:43 +0100

    commit 3

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-document.addEventListener("submit",(event)=>{
-    console.log("submitted")
-    user = document.getElementById("user").value
-    password = document.getElementById("password").value
+const cookieStorage = {
+    getItem: (item) =>{
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc,[key,value]) =>({...acc,[key.trim()]: value}));
+            return cookies[item]
+    },
+    setItem:(key, value)=>{
+        document.cookie = `${key}=${value}`;
+    },
+};
 
-    const headers = new Headers()
-    headers.append("Accept", "application/json")
-    headers.append("Authorization",btoa(user + ":" + password))
+const storageType = localStorage;
+const consentPropertyName = 'jdc_consent';
+
+const shouldShowPopup = () => ! storageType.getItem(consentPropertyName);
+const saveToStorage =()=> storageType.setItem(consentPropertyName,true);
+
+window.onload = ()=>{
+    if(shouldShowPopup()){
+        const consent = confirm('Agree to the terms and conditions of the site?');
+        if(consent){
+            saveToStorage();
+        }
+    }
+}
+
+document.addEventListener("submit" , (event)=>{
+    console.log("Validating")
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+
+    let request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:3000/authorize");
+    //request.send(new FormData(formElement));
+    console.log("Validated")
+
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(`user=${user}&password=${password}`);
 
     event.preventDefault()
 })
-
-function validate(){
-    console.log("Hello World");
-    let user = document.getElementById("user").value
-    let password = document.getElementById("password").value
-    console.log(`User = ${user} Password = ${password}`)
-
-    const XHR = new XMLHttpRequest(), FD = new FormData();
-    FD.append(name,data [user]);
-    FD.append(name,data [password]);
-}
