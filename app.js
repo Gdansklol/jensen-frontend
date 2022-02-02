89db907a3d4979d9d538f105a2d3cd98f7fb971d
Båmi Lee <swedenebomi@gmail.com>
2022-02-02 20:33:26 +0100

    with recaptcha

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
-const cookieStorage = {
+ const cookieStorage = {
     getItem: (item) =>{
         const cookies = document.cookie
             .split(';')
             .map(cookie => cookie.split('='))
-            .reduce((acc,[key,value]) =>({...acc,[key.trim()]: value}));
-            return cookies[item]
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+            return cookies[item];
     },
     setItem:(key, value)=>{
         document.cookie = `${key}=${value}`;
     },
 };
 
-const storageType = localStorage;
+const storageType = cookieStorage;
 const consentPropertyName = 'jdc_consent';
-
 const shouldShowPopup = () => ! storageType.getItem(consentPropertyName);
 const saveToStorage =()=> storageType.setItem(consentPropertyName,true);
 
-window.onload = ()=>{
-    if(shouldShowPopup()){
-        const consent = confirm('Agree to the terms and conditions of the site?');
-        if(consent){
-            saveToStorage();
-        }
+function code(){
+    alert("Agree to the terms and conditions of the site?");
     }
-}
+    
+    window.onload=code();
+
+
+  //window.onload = ()=>{
+
+    
+    const acceptFn = event =>{
+        saveToStorage(storageType);
+        //consentPopup.classList.add('hidden');
+    //}
+    const consentPopup = document.getElementById('consent-popup')
+    const acceptBtn = document.getElementById('accept');
+    acceptBtn.addEventListener('click',acceptFn);
+    
+    if(shouldShowPopup(storageType)){
+        setTimeout(() => {
+            consentPopup.classList.remove('hidden');
+        },2000);
+    }
+};
+
 
 document.addEventListener("submit" , (event)=>{
     console.log("Validating")
@@ -32,7 +48,7 @@ document.addEventListener("submit" , (event)=>{
     let password = document.getElementById("password").value;
 
     let request = new XMLHttpRequest();
-    request.open("POST", "http://localhost:3000/authorize");
+    request.open("POST", "https://bomi-backend.herokuapp.com/authorize");
     //request.send(new FormData(formElement));
     console.log("Validated")
 
@@ -41,3 +57,7 @@ document.addEventListener("submit" , (event)=>{
 
     event.preventDefault()
 })
+
+
+
+
